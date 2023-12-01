753705c058c9ffb189571974170f05cb467fb8c7
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2023-12-01 11:14:04 +0100

    Part 1 - day 1

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,3 +1,24 @@
+import re
+
 
 def get_values_for_calibration(data):
-    return 0
+    numbers = re.findall(r'\d', data)
+    return int(f"{numbers[0]}{numbers[-1]}")
+
+
+def get_calibration_sum(data):
+    data_list = [y for y in (x.strip() for x in data.splitlines()) if y]
+    cal_list = []
+    for elem in data_list:
+        cal_list.append(get_values_for_calibration(elem))
+
+    print(cal_list)
+    return sum(cal_list)
+
+
+##### PART 1 #####
+
+with open('puzzle_input.txt', 'r') as file:
+    puzzle_input = file.read()
+print(puzzle_input)
+print(get_calibration_sum(puzzle_input))
--- day_1/test_day_1.py
+++ day_1/test_day_1.py
@@ -1,6 +1,7 @@
 import pytest
 
-from day_1 import get_values_for_calibration
+from day_1 import get_values_for_calibration, get_calibration_sum
+
 
 @pytest.mark.parametrize("data,expected", [("1abc2", 12),("pqr3stu8vwx", 38),("a1b2c3d4e5f", 15),("treb7uchet", 77)])
 def test_calibration_values(data,expected):
@@ -9,3 +10,15 @@ def test_calibration_values(data,expected):
     result = get_values_for_calibration(data)
     # THEN
     assert expected == result
+
+
+def test_calibration_sum():
+    # GIVEN
+    data = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
+    # WHEN
+    result = get_calibration_sum(data)
+    # THEN
+    assert 142 == result
